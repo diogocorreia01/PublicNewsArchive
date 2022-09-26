7353ab7a2c4d9c1d90a18afe977ca5d037e781e9
diogocorreia01 <diogo.correia01@outlook.com>
2022-09-26 21:40:27 +0100

    Update newsData

---
+++ publicnewsarchive/__init__.py
@@ -0,0 +1,2 @@
+from publicnewsarchive import newsData
+from publicnewsarchive import newsAnalysis
---
+++ publicnewsarchive/newsData/__init__.py
@@ -0,0 +1,3 @@
+from publicnewsarchive.newsData.getFontPages import getFontPages
+from publicnewsarchive.newsData.getNewsArticles import getNewsArticles
+from publicnewsarchive.newsData.getNewsData import getNewsData
