3a5dcfcf1a7fc166362406927cf85a12d2018805
diogocorreia01 <diogo.correia01@outlook.com>
2022-10-18 22:25:29 +0100

    update Scraping

---
+++ Scraping/CorreioManha.py
@@ -0,0 +1,17 @@
+from publicnewsarchive import newsData
+
+#2022
+
+listOfPastURLs = newsData.getPastURLs(year='2022', newspaper_url='https://www.cmjornal.pt/')
+
+newsData.getNewsArticles(pastURLs=listOfPastURLs, news_htmlTag='div', news_htmlClass='text_container', titles_htmlTag='a', titles_htmlClass='eventAnalytics',
+                         snippets_htmlTag='p',snippets_htmlClass='destaques_lead', links_htmlTag='a', links_htmlClass='eventAnalytics',
+                         authors_htmlTag='', authors_htmlClass='', output_path='samples\\newsJornalNoticias2022', debug=True)
+
+#2021
+
+listOfPastURLs = newsData.getPastURLs(year='2021', newspaper_url='https://www.cmjornal.pt/')
+
+newsData.getNewsArticles(pastURLs=listOfPastURLs, news_htmlTag='div', news_htmlClass='text_container', titles_htmlTag='a', titles_htmlClass='eventAnalytics',
+                         snippets_htmlTag='p',snippets_htmlClass='destaques_lead', links_htmlTag='a', links_htmlClass='eventAnalytics',
+                         authors_htmlTag='', authors_htmlClass='', output_path='samples\\newsJornalNoticias2021', debug=True)
