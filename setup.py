02657d1d4873805242008021cfa5d31b9cd24cb1
diogocorreia01 <diogo.correia01@outlook.com>
2022-10-21 10:54:03 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -22,19 +22,19 @@ setup(
     license='LGPLv3',
     include_package_data=True,
     install_requires=[
-        'beautifulsoup4==4.11.1'
-        'bs4==0.0.1'
-        'date-guesser-rc @ git+https://github.com/rncampos/date_guesser@4007399df72fbfba18ab1c9852153cd80eb616e9'
-        'geopy==2.2.0'
-        'gmplot==1.4.1'
-        'matplotlib==3.5.0'
-        'numpy==1.23.3'
-        'pandas==1.5.0'
-        'pt-core-news-lg @ https://github.com/explosion/spacy-models/releases/download/pt_core_news_lg-3.3.0/pt_core_news_lg-3.3.0-py3-none-any.whl'
-        'requests==2.28.1'
-        'spacy==3.3.0'
-        'urllib3==1.26.12'
-        'wordcloud==1.8.2.2'
+        'beautifulsoup4==4.11.1',
+        'bs4==0.0.1',
+        'date-guesser-rc @ git+https://github.com/rncampos/date_guesser@4007399df72fbfba18ab1c9852153cd80eb616e9',
+        'geopy==2.2.0',
+        'gmplot==1.4.1',
+        'matplotlib==3.5.0',
+        'numpy==1.23.3',
+        'pandas==1.5.0',
+        'pt-core-news-lg @ https://github.com/explosion/spacy-models/releases/download/pt_core_news_lg-3.3.0/pt_core_news_lg-3.3.0-py3-none-any.whl',
+        'requests==2.28.1',
+        'spacy==3.3.0',
+        'urllib3==1.26.12',
+        'wordcloud==1.8.2.2',
         'yake @ git+https://github.com/LIAAD/yake@6a37efc67912195aebed562cd943bca8acc48f64'
     ]
 )
